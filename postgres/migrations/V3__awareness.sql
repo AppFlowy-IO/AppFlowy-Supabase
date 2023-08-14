bb486ae44c8c50c21103b7df966e2b2c3684bba3
nathan <nathan@appflowy.io>
2023-08-14 23:11:02 +0800

    fix: disable update policy for af_user table

--- postgres/migrations/V3__awareness.sql
+++ postgres/migrations/V3__awareness.sql
@@ -4,3 +4,9 @@ VALUES IN (5);
 -- Add the encrypt column to the af_collab_update table. If the encrypt column is
 -- null, then the update is not encrypted.
 ALTER TABLE af_collab_update ADD COLUMN encrypt INTEGER DEFAULT null;
+ALTER TABLE af_user ADD COLUMN encrypt INTEGER DEFAULT null;
+
+ALTER POLICY af_user_update_policy
+ON public.af_user
+USING (true)
+WITH CHECK (true);
