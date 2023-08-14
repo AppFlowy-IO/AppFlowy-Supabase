35d337d3e603ba1c8f10fa25e1a5bc18857be9c8
nathan <nathan@appflowy.io>
2023-08-14 11:36:11 +0800

    chore: update

--- postgres/migrations/V3__awareness.sql
+++ postgres/migrations/V3__awareness.sql
@@ -1,4 +1,6 @@
 CREATE TABLE af_collab_update_user_awareness PARTITION OF af_collab_update FOR
 VALUES IN (5);
 
-ALTER TABLE af_collab_update ADD COLUMN is_encrypt BOOLEAN DEFAULT false;
+-- Add the encrypt column to the af_collab_update table. If the encrypt column is
+-- null, then the update is not encrypted.
+ALTER TABLE af_collab_update ADD COLUMN encrypt INTEGER DEFAULT null;
